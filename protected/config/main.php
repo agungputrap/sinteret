64f0badfa17598f6708d33baab403d4737d3818f
agungputrap <real.stereo2@gmail.com>
2017-07-04 23:36:14 +0700

    baru kopas eauth dan teman temannya

--- protected/config/main.php
+++ protected/config/main.php
@@ -16,6 +16,11 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+        'application.extensions.eoauth.*',
+        'application.extensions.eoauth.lib.*',
+        'application.extensions.lightopenid.*',
+        'application.extensions.eauth.*',
+        'application.extensions.eauth.services.*',
 	),
 
 	'modules'=>array(
@@ -37,6 +42,122 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
+
+        'loid' => array(
+            //alias to dir, where you unpacked extension
+            'class' => 'application.extensions.lightopenid.loid',
+        ),
+
+        'eauth' => array(
+            'class' => 'application.extensions.eauth.EAuth',
+            'popup' => true, // Use the popup window instead of redirecting.
+            'cache' => false, // Cache component name or false to disable cache. Defaults to 'cache'.
+            'cacheExpire' => 0, // Cache lifetime. Defaults to 0 - means unlimited.
+            'services' => array( // You can change the providers and their classes.
+                'steam' => array(
+                    'class' => 'SteamOpenIDService',
+                    //'realm' => '*.example.org',
+                ),
+                'yahoo' => array(
+                    'class' => 'YahooOpenIDService',
+                    //'realm' => '*.example.org',
+                ),
+                'wargaming' => array(
+                    'class' => 'WargamingOpenIDService'
+                ),
+                'twitter' => array(
+                    // register your app here: https://dev.twitter.com/apps/new
+                    'class' => 'TwitterOAuthService',
+                    'key' => '...',
+                    'secret' => '...',
+                ),
+                'google_oauth' => array(
+                    // register your app here: https://code.google.com/apis/console/
+                    'class' => 'GoogleOAuthService',
+                    'client_id' => '...',
+                    'client_secret' => '...',
+                    'title' => 'Google (OAuth)',
+                ),
+                'yandex_oauth' => array(
+                    // register your app here: https://oauth.yandex.ru/client/my
+                    'class' => 'YandexOAuthService',
+                    'client_id' => '...',
+                    'client_secret' => '...',
+                    'title' => 'Yandex (OAuth)',
+                ),
+                'facebook' => array(
+                    // register your app here: https://developers.facebook.com/apps/
+                    'class' => 'FacebookOAuthService',
+                    'client_id' => '...',
+                    'client_secret' => '...',
+                ),
+                'linkedin' => array(
+                    // register your app here: https://www.linkedin.com/secure/developer
+                    'class' => 'LinkedinOAuthService',
+                    'key' => '...',
+                    'secret' => '...',
+                ),
+                'github' => array(
+                    // register your app here: https://github.com/settings/applications
+                    'class' => 'GitHubOAuthService',
+                    'client_id' => '...',
+                    'client_secret' => '...',
+                ),
+                'live' => array(
+                    // register your app here: https://manage.dev.live.com/Applications/Index
+                    'class' => 'LiveOAuthService',
+                    'client_id' => '...',
+                    'client_secret' => '...',
+                ),
+                'vkontakte' => array(
+                    // register your app here: https://vk.com/editapp?act=create&site=1
+                    'class' => 'VKontakteOAuthService',
+                    'client_id' => '...',
+                    'client_secret' => '...',
+                ),
+                'mailru' => array(
+                    // register your app here: http://api.mail.ru/sites/my/add
+                    'class' => 'MailruOAuthService',
+                    'client_id' => '...',
+                    'client_secret' => '...',
+                ),
+                'moikrug' => array(
+                    // register your app here: https://oauth.yandex.ru/client/my
+                    'class' => 'MoikrugOAuthService',
+                    'client_id' => '...',
+                    'client_secret' => '...',
+                ),
+                'odnoklassniki' => array(
+                    // register your app here: http://dev.odnoklassniki.ru/wiki/pages/viewpage.action?pageId=13992188
+                    // ... or here: http://www.odnoklassniki.ru/dk?st.cmd=appsInfoMyDevList&st._aid=Apps_Info_MyDev
+                    'class' => 'OdnoklassnikiOAuthService',
+                    'client_id' => '...',
+                    'client_public' => '...',
+                    'client_secret' => '...',
+                    'title' => 'Odnokl.',
+                ),
+                'dropbox' => array(
+                    // register your app here: https://www.dropbox.com/developers/apps/create
+                    'class' => 'DropboxOAuthService',
+                    'client_id' => '...',
+                    'client_secret' => '...',
+                ),
+                'eve' => array(
+                    // register your app here: https://developers.eveonline.com/applications
+                    'class' => 'EveOnlineOAuthService',
+                    'client_id' => '...',
+                    'client_secret' => '...',
+                ),
+                'slack' => array(
+                    // register your app here: https://api.slack.com/applications/new
+                    'class' => 'SlackOAuthService',
+                    'client_id' => '...',
+                    'client_secret' => '...',
+                    'title' => 'Slack',
+                ),
+
+            ),
+        ),
 
 		// uncomment the following to enable URLs in path-format
 		/*
@@ -66,11 +187,9 @@ return array(
 					'levels'=>'error, warning',
 				),
 				// uncomment the following to show log messages on web pages
-				/*
 				array(
 					'class'=>'CWebLogRoute',
 				),
-				*/
 			),
 		),
 
--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -48,6 +48,16 @@ $this->breadcrumbs=array(
 	<div class="row buttons">
 		<?php echo CHtml::submitButton('Login'); ?>
 	</div>
-
+    <hr/>
+    <?php
+    if (Yii::app()->user->hasFlash('error')) {
+        echo '<div class="error">'.Yii::app()->user->getFlash('error').'</div>';
+    }
+    ?>
+    ...
+    <h2>Do you already have an account on one of these sites? Click the logo to log in with it here:</h2>
+    <?php
+    $this->widget('application.extensions.eauth.EAuthWidget', array('action' => 'site/login'));
+    ?>
 <?php $this->endWidget(); ?>
 </div><!-- form -->
