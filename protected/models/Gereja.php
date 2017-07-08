9e87efb92becdc8b783bb584165089189eab4b90
agungpp <real.stereo2@gmail.com>
2017-07-08 12:10:25 +0700

    ketinggalan commit fix conflict

--- protected/models/Gereja.php
+++ protected/models/Gereja.php
@@ -51,17 +51,10 @@ class Gereja extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-<<<<<<< HEAD
 			'fotoGerejas' => array(self::HAS_MANY, 'FotoGereja', 'gereja_id'),
 			'historyGerejaViews' => array(self::HAS_MANY, 'HistoryGerejaView', 'gereja_id'),
 			'jadwalIbadahs' => array(self::HAS_MANY, 'JadwalIbadah', 'gereja_id'),
 			'tipe' => array(self::BELONGS_TO, 'TipeGereja', 'tipe_id'),
-=======
-			'jadwalIbadahs' => array(self::HAS_MANY, 'JadwalIbadah', 'gereja_id'),
-			'fotoGerejas' => array(self::HAS_MANY, 'FotoGereja', 'gereja_id'),
-			'tipe' => array(self::BELONGS_TO, 'TipeGereja', 'tipe_id'),
-			'historyGerejaViews' => array(self::HAS_MANY, 'HistoryGerejaView', 'gereja_id'),
->>>>>>> fd73a2b04fc3b0f3631c8f322adb18424326c979
 		);
 	}
 
