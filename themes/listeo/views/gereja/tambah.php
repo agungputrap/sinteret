18c2eec4d9d5125624b854de73dbd82c37b85ccd
agungpp <real.stereo2@gmail.com>
2017-08-10 15:54:49 +0700

    dari http ke https

--- themes/listeo/views/gereja/tambah.php
+++ themes/listeo/views/gereja/tambah.php
@@ -124,7 +124,7 @@
 </div>
 <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyDJhgUHr15-NnoWV3kFBNNnaLlo8_oCI7Q&amp;language=id&amp;libraries=places"></script>
+<script type="text/javascript" src="https://maps.google.com/maps/api/js?key=AIzaSyDJhgUHr15-NnoWV3kFBNNnaLlo8_oCI7Q&amp;language=id&amp;libraries=places"></script>
 <script src="<?php echo Yii::app()->theme->baseUrl; ?>/sources/scripts/locationpicker.jquery.js"></script>
 <script>
     $('#us3').locationpicker({
