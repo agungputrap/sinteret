6198b0ccd87f6560ae2e954014c3d35722fa1039
agungpp <real.stereo2@gmail.com>
2017-08-10 15:23:59 +0700

    ganti db

--- protected/config/database.php
+++ protected/config/database.php
@@ -4,9 +4,9 @@
 return array(
 //	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
     // uncomment the following lines to use a MySQL database
-    'connectionString' => 'pgsql:host=localhost;dbname=Gemana',
+    'connectionString' => 'pgsql:host=ec2-107-20-250-195.compute-1.amazonaws.com;dbname=dfjvb7l9db0g17',
     'emulatePrepare' => true,
-    'username' => 'postgres',
-    'password' => 'postgres',
+    'username' => 'drilvyduqcescc',
+    'password' => 'cdcc2dde25f9c6dcaac7709ab149d9ab0f494b1d87d99d8d92d88546089a9dd5',
     'charset' => 'utf8',
 );
