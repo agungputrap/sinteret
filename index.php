5ba1f3ac9c9593422e60cacb382ce5a2cc9fc6ef
agungpp <real.stereo2@gmail.com>
2017-08-10 14:00:05 +0700

    push kosong biar ke deploy

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 // change the following paths if necessary
+//coba push ke testing agung lagi buat deploy
 $yii=dirname(__FILE__).'/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
